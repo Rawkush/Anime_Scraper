71f71604f56fd94b665e9dc2fe7ab83969f3e681
MauryaD <ankushrawat950@gmail.com>
2018-11-08 16:38:06 +0530

    project skeleton created

--- src/com/gecdevelopers/scrapper/Scrapper.java
+++ src/com/gecdevelopers/scrapper/Scrapper.java
@@ -1,5 +1,28 @@
 package com.gecdevelopers.scrapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scrapper {
 
+	
+    ArrayList<AnimeModel> recentAnimeList;
+    ArrayList<AnimeModel> popularOngoingAnimeList; // ongoing and popular
+    
+    public Scrapper() {
+    	recentAnimeList= new ArrayList<>();
+    	popularOngoingAnimeList= new ArrayList<>();
+    }
+	
+	
+	public ArrayList<AnimeModel> getRecentAnimeList(){
+		return recentAnimeList;
+	}
+	
+	public ArrayList<AnimeModel> getPopularOngoindAnimeList(){
+		return popularOngoingAnimeList;
+	}
+	
+	
+	
 }
