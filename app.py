9dda4b5445d426b2b620d45b82deec5251d77cd0
Ankush <ankushrawat950@gmail.com>
2019-11-13 22:08:12 +0530

    changing parser

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ BASE_URL="https://www.gogoanime.io/"
 
 def getNew():
     page = requests.get(BASE_URL)
-    soup = BeautifulSoup(page.content, 'html5lib') 
+    soup = BeautifulSoup(page.content, "html.parser") 
     table = soup.find('div', attrs = {'class':'last_episodes loaddub'}).find_all('li')
     data=[ {'url':r.div.a['href'],'title':r.div.a['title'],'img':r.div.img['src'], 'episode':r.find('p','episode').text}     for r in table]
     return json.dumps(data)
@@ -16,7 +16,7 @@ def getNew():
 def getAnime( url):
     URL=BASE_URL+"category" + url.split("-episode")[0]
     page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html5lib') 
+    soup = BeautifulSoup(page.content, "html.parser") 
     ep=soup.find('ul',attrs={'id':'episode_page'})
     url=URL.split('category/')
     url=url[0]+url[1]
@@ -28,7 +28,7 @@ def getAnime( url):
 def getEpisode( url,  ep_num):
     url + "-episode-"  + ep_num
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html5lib') 
+    soup = BeautifulSoup(page.content, "html.parser") 
     ep=soup.find('div',attrs={'class':'anime_muti_link'}).find_all('li')
     down=soup.find('div',attrs={'class':'anime_video_body_cate'}).find_all('a')
     data={'stream':[{l['class'][0]:l.a['data-video']} for l in ep ], "download":down[-1]['href']}
@@ -39,7 +39,7 @@ def search(anime):
     URL=BASE_URL+'search.html?keyword='
     anime= URL+anime
     page = requests.get(anime)
-    soup = BeautifulSoup(page.content, 'html5lib') 
+    soup = BeautifulSoup(page.content, "html.parser") 
     table = soup.find('div', attrs = {'class':'last_episodes'}).find_all('li')
     data=[ {'title':x.find('p','name').a['title'], 'img':x.div.a.img['src'],'url':x.div.a['href'],'released': x.find('p','released').text.strip()}   for x in table]
     return json.dumps(data)
